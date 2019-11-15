983d82b520fc27b4fd204af95047b1997856cde2
muralim <muralim@W2012R2-BENCH06.mavericsystems.lan>
2019-11-15 12:26:41 +0530

    Commit 3(path issue )

--- src/test/java/org/runner/TestRunner.java
+++ src/test/java/org/runner/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
  
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Features\\log.feature",glue={"org/step"})
+@CucumberOptions(features = "src/test/resources/Features/log.feature",glue={"org/step"})
  
 public class TestRunner {
  
